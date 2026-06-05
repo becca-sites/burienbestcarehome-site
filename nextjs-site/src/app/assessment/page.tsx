b0e3dce93b6e75b20993c6c956be1e54835de19b
YourNextStepTeam <becca@yournextstepteam.com>
2026-06-05 15:23:56 -0400

    Add files via upload

--- nextjs-site/src/app/assessment/page.tsx
+++ nextjs-site/src/app/assessment/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import Link from 'next/link'
 
 // The care quiz moved from /assessment to /is-it-time.
 // This client redirect keeps old links and bookmarks working.
@@ -12,7 +13,7 @@ export default function AssessmentRedirect() {
   return (
     <p style={{ padding: '8rem 1.5rem', textAlign: 'center' }}>
       This page has moved. Redirecting you to{' '}
-      <a href="/is-it-time">Is It Time?</a>
+      <Link href="/is-it-time">Is It Time?</Link>
     </p>
   )
 }
