4a0adc01a55d74ef154f60f9ded1583e4b773bc2
YourNextStepTeam <becca@yournextstepteam.com>
2026-06-17 17:12:20 -0400

    FIX: add moveTimeline to form reset object (TS type error)

--- nextjs-site/src/components/ContactForm.tsx
+++ nextjs-site/src/components/ContactForm.tsx
@@ -154,6 +154,7 @@ export default function ContactForm() {
         careType: '',
         visitDate: '',
         visitTime: '',
+        moveTimeline: '',
         message: '',
         botcheck: '',
       })
